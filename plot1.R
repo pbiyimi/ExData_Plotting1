4a68efb69fd5f111cfb29556d7c080b279ca145d
Perseverance Biyimi Fouanamio <p.biyimi@hotmail.fr>
2015-01-12 01:19:07 +0100

    with png()

--- plot1.R
+++ plot1.R
@@ -1,1 +1,3 @@
+png("plot1.png",width = 480, height = 480)
 hist(data[,3], c= "orangered2", freq = T, main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+dev.off()
--- plot2.R
+++ plot2.R
@@ -1,2 +1,3 @@
+png("plot2.png",width = 480, height = 480)
 plot(dates,data[,3], ylab = "Global Active Power(kilowatts)", type = "l", xlab = "")
-
+dev.off()
